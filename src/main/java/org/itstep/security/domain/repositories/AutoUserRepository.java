5fe89d85589f8e642e78382ec60655840913d0a1
Maxim <maxshaptala@gmail.com>
2017-12-20 02:01:02 +0200

    added repository

--- src/main/java/org/itstep/security/domain/repositories/AutoUserRepository.java
+++ src/main/java/org/itstep/security/domain/repositories/AutoUserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AutoUserRepository extends JpaRepository<AutoUser, Long> {
 
+    public AutoUser findUserByUsername(String username);
+
 }
